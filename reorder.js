105ef5a3bd5c045ccde0a56591b1cf32e617eb1c
rob <rgw20@ic.ac.uk>
2022-02-17 00:32:56 +0000

    Add extension signing

--- reorder.js
+++ reorder.js
@@ -1,15 +1,14 @@
-function ready() { 
-  console.log("reordering")
+function ready() {
   tbody = document.getElementsByTagName('tbody')[0]
   rows = Array.prototype.slice.call(tbody.children)
-  console.log(rows)
+  //console.log(rows)
   for (let i = 0; i < rows.length; i++) {
     tbody.removeChild(rows[i])
   }
   for (let i = 1; i <= rows.length; i++) {
     tbody.appendChild(rows[rows.length - i])
   }
+  document.getElementsByTagName('h2')[0].textContent += "(Reordered)"
 }
 
-console.log("url match")
 ready()
